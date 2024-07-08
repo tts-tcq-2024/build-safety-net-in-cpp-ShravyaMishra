ffe5b5dcf2ddb4f142fc6feb325f25b6c8366a05
ShravyaMishra <167961412+ShravyaMishra@users.noreply.github.com>
2024-07-08 14:16:10 +0530

    Update Soundex.h

--- Soundex.h
+++ Soundex.h
@@ -3,6 +3,14 @@
 
 #include <string>
 
-std::string generateSoundex(const std::string& name);
+class Soundex {
+public:
+    static char getSoundexCode(char c);
+    static bool isHW(char c);
+    static bool shouldAppend(char currentCode, char lastCode, char nextChar);
+    static std::string accumulateSoundexCodes(const std::string& name);
+    static std::string padSoundex(const std::string& soundex);
+    static std::string generateSoundex(const std::string& name);
+};
 
 #endif // SOUNDEX_H
