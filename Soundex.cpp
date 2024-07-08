800ecf9ca6c0cc9c09e80c2c33534a059848739f
ShravyaMishra <167961412+ShravyaMishra@users.noreply.github.com>
2024-07-08 14:14:23 +0530

    Update Soundex.cpp

--- Soundex.cpp
+++ Soundex.cpp
@@ -1,36 +1,81 @@
 #include "Soundex.h"
-#include <cctype>
 
-char getSoundexCode(char c) {
-    c = toupper(c);
-    switch (c) {
-        case 'B': case 'F': case 'P': case 'V': return '1';
-        case 'C': case 'G': case 'J': case 'K': case 'Q': case 'S': case 'X': case 'Z': return '2';
-        case 'D': case 'T': return '3';
-        case 'L': return '4';
-        case 'M': case 'N': return '5';
-        case 'R': return '6';
-        default: return '0'; // For A, E, I, O, U, H, W, Y
+char Soundex::getSoundexCode(char c) {
+    switch (std::tolower(c)) {
+        case 'b':
+        case 'f':
+        case 'p':
+        case 'v':
+            return '1';
+        case 'c':
+        case 'g':
+        case 'j':
+        case 'k':
+        case 'q':
+        case 's':
+        case 'x':
+        case 'z':
+            return '2';
+        case 'd':
+        case 't':
+            return '3';
+        case 'l':
+            return '4';
+        case 'm':
+        case 'n':
+            return '5';
+        case 'r':
+            return '6';
+        default:
+            return '0';
     }
 }
 
-std::string generateSoundex(const std::string& name) {
-    if (name.empty()) return "";
+bool Soundex::isHW(char c) {
+    return std::tolower(c) == 'h' || std::tolower(c) == 'w';
+}
 
-    std::string soundex(1, toupper(name[0]));
-    char prevCode = getSoundexCode(name[0]);
+bool Soundex::shouldAppend(char currentCode, char lastCode, char nextChar) {
+    if (currentCode == '0')
+        return true;
+    if (currentCode == lastCode)
+        return false;
+    if (isHW(nextChar))
+        return false;
+    return true;
+}
 
-    for (size_t i = 1; i < name.length() && soundex.length() < 4; ++i) {
+std::string Soundex::accumulateSoundexCodes(const std::string& name) {
+    if (name.empty())
+        return "0000";
+
+    std::string soundexCodes;
+    soundexCodes += std::toupper(name[0]);
+    char lastCode = getSoundexCode(name[0]);
+
+    for (size_t i = 1; i < name.length() && soundexCodes.length() < 4; ++i) {
         char code = getSoundexCode(name[i]);
-        if (code != '0' && code != prevCode) {
-            soundex += code;
-            prevCode = code;
+        if (shouldAppend(code, lastCode, name[i])) {
+            soundexCodes += code;
+            lastCode = code;
         }
     }
 
-    while (soundex.length() < 4) {
-        soundex += '0';
-    }
+    while (soundexCodes.length() < 4)
+        soundexCodes += '0';
 
-    return soundex;
+    return soundexCodes;
 }
+
+std::string Soundex::padSoundex(const std::string& soundex) {
+    std::string paddedSoundex = soundex;
+    while (paddedSoundex.length() < 4)
+        paddedSoundex += '0';
+    return paddedSoundex.substr(0, 4);
+}
+
+std::string Soundex::generateSoundex(const std::string& name) {
+    std::string accumulatedCodes = accumulateSoundexCodes(name);
+    return padSoundex(accumulatedCodes);
+}
+
